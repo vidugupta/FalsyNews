0b3ba3b7c7be67124faf1a8a8a5e2b38e2ab50fe
Vidushi Gupta <vidugupta1931@gmail.com>
2022-05-16 11:56:35 +0530

    Check component added
    New component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { CheckComponent } from './check/check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
